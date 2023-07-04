5016960e530a071ea54e77cbc6b21a7e2335289c
Piyush Kumar <136954213+kr-piyush@users.noreply.github.com>
2023-07-04 10:26:13 +0530

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+let addChat =  document.querySelector(".list-container")
+let addChatCont = document.querySelector("left-main-top")
+let exampsC = document.querySelectorAll(".examp")
+
+addChat.addEventListener("click", function() {
+alert("add new chat")
+})
+
+exampsC.addEventListener("click", function() {
+    alert("push to input")
+})
